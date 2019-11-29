eb6be42f5baeb1842e02a11dd79f5d44ea0fc736
TTOzzi <devttozzi@gmail.com>
2019-11-29 20:02:52 +0900

    팀 데이터 출력 구현, 잘못 입력했을 시 에러문구 출력 구현
    데이터를 매개변수로 받아 출력하는 printData 함수
      ㄴ	forEach 를 활용하여 팀의 모든 요소들을 출력
    
    초반 메뉴 선택에 오류처리 추가
      ㄴ	데이터가 이미 입력된 상태에서 데이터 입력 선택시 오류메세지 출력
      ㄴ	1, 2 가 아닌 다른 값이 입력되었을 때 오류메세지 출력

--- TestStep2/TestStep2/main.swift
+++ TestStep2/TestStep2/main.swift
@@ -24,11 +24,15 @@ func main() {
         print("신나는 야구시합\n1. 데이터 입력\n2. 데이터 출력\n")
         print("메뉴 선택 (1 - 2)")
         let input = readLine()!
-        if input == "1" {
+        if input == "1", teams.count == 0 {
             teams = inputData()
-        } else {
-            print("2")
+        } else if input == "1" {
+            print("이미 데이터를 입력하셨습니다.\n")
+        } else if input == "2" {
+            printData(teams)
             break
+        } else {
+            print("잘못된 값을 입력하셨습니다.\n")
         }
     }
 }
@@ -39,7 +43,7 @@ func inputData() -> [Team] {
         print("\(i)팀의 이름을 입력하세요>")
         let teamName = readLine()!
         var players = [Hitter]()
-        for j in 1...3 {    // 9명 입력으로 수정하기
+        for j in 1...9 {
             print("\(j)번 타자 정보 입력>")
             let input = readLine()!.components(separatedBy: ", ")
             players.append(Hitter(number: j, name: input[0], h: Double(input[1])!))
@@ -48,6 +52,16 @@ func inputData() -> [Team] {
     }
     print("팀 데이터 입력이 완료되었습니다\n")
     return ret
+}
+
+func printData(_ team: [Team]) {
+    team.forEach {
+        print("\($0.teamName) 팀 정보")
+        $0.players.forEach {
+            print("\($0.number)번 \($0.name), \($0.h)")
+        }
+        print("\n")
+    }
 }
 
 main()

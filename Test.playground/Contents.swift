ce9c5f4a97fc8ccbdca62c2582639befc788bf09
TTOzzi <devttozzi@gmail.com>
2019-11-29 14:30:46 +0900

    점수출력 구현
    클래스 내부에 printScore 함수를 만들어 점수 출력
    
    볼 4회 누적시 1안타 추가
    
    게임 종료 시 최종 안타 수 출력

--- Test.playground/Contents.swift
+++ Test.playground/Contents.swift
@@ -8,13 +8,23 @@ class Score {
         self.strike = 0
         self.ball = 0
     }
+    
+    func printScore() {
+        print("\(self.strike)S \(self.ball)B \(self.out)O\n")
+    }
 }
 
 func game() {
     let score = Score()
+    
+    print("신나는 야구 게임!")
+    print("첫 번째 타자가 타석에 입장했습니다.\n")
     while score.out < 3 {
         throwBall(Int.random(in: 1...4), score)
+        score.printScore()
     }
+    print("최종 안타수: \(score.hit)")
+    print("GAME OVER")
 }
 
 func throwBall(_ n: Int, _ s: Score) {
@@ -23,15 +33,20 @@ func throwBall(_ n: Int, _ s: Score) {
         s.strike += 1
         print("스트라이크!")
         if s.strike == 3 {
-            s.nextHitter()
             s.out += 1
-            print("아웃!")
+            if s.out < 3 {
+                s.nextHitter()
+                print("아웃! 다음 타자가 타석에 입석했습니다.")
+            } else {
+                print("아웃!")
+            }
         }
     case 2:
         s.ball += 1
         print("볼!")
         if s.ball == 4 {
             s.nextHitter()
+            s.hit += 1
             print("볼넷!")
         }
     case 3:

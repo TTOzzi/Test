4b0d1d60213f914e8b82a9e0f1c2e1a9d443ecb4
TTOzzi <devttozzi@gmail.com>
2019-11-29 14:07:38 +0900

    전체적인 게임 로직 구성
    Score 클래스를 만들어 점수 저장
      ㄴ 딕셔너리나 배열로 저장하는 방법도 생각해봤으나 함수로 기능을 나눠 저장하기엔 클래스가 제일 적합하다고 생각
    
    1 부터 4 까지의 무작위 수를 입력받아 그에 맞는 결과를 저장
      ㄴ 1: 스트라이크, 2: 볼, 3: 안타, 4: 아웃 으로 임의로 지정함
    
    중복코드 제거 및 점수출력 구현 필요

--- Test.playground/Contents.swift
+++ Test.playground/Contents.swift
@@ -1,1 +1,53 @@
+class Score {
+    var strike = 0
+    var ball = 0
+    var out = 0
+    var hit = 0
+}
 
+func game() {
+    let score = Score()
+    while score.out < 3 {
+        throwBall(Int.random(in: 1...4), score)
+    }
+}
+
+func throwBall(_ n: Int, _ s: Score) {
+    switch n {
+    case 1:
+        s.strike += 1
+        print("스트라이크!")
+        if s.strike == 3 {
+            s.strike = 0
+            s.ball = 0
+            s.out += 1
+            print("아웃!")
+        }
+    case 2:
+        s.ball += 1
+        print("볼!")
+        if s.ball == 4 {
+            s.strike = 0
+            s.ball = 0
+            print("볼넷!")
+        }
+    case 3:
+        s.hit += 1
+        s.strike = 0
+        s.ball = 0
+        print("안타!")
+    case 4:
+        s.out += 1
+        s.strike = 0
+        s.ball = 0
+        if s.out < 3 {
+            print("아웃! 다음 타자가 타석에 입석했습니다.")
+        } else {
+            print("아웃!")
+        }
+    default:
+        break
+    }
+}
+
+game()
